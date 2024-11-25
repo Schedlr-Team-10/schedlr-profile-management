5fbcd5810fd4aa2112493036b3914e2705623eda
Ravi Shankar <Bhangeravishankar2302@gmail.com>
2024-11-25 00:40:22 -0500

    updated the user.java

--- src/main/java/com/dm/projectSpring/Entity/User.java
+++ src/main/java/com/dm/projectSpring/Entity/User.java
@@ -1,11 +1,6 @@
 package com.dm.projectSpring.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -32,66 +27,17 @@ public class User {
     private String password;
 
     @Enumerated(EnumType.STRING)
-@Column(nullable = false)
-private AccountType accountType;
+    @Column(nullable = false)
+    private AccountType accountType;
 
-public enum AccountType {
-    INFLUENCER, PERSONAL
-}
+    @Lob
+    private byte[] profilepic;
 
-    public AccountType getAccountType() {
-        return accountType;
+    @Lob
+    private String bio;
+
+    public enum AccountType {
+        INFLUENCER, PERSONAL
     }
 
-    public void setAccountType(AccountType accountType) {
-        this.accountType = accountType;
-    }
-    private String otp;
-
-    public int getUserid() {
-        return userid;
-    }
-
-    public void setUserid(int userid) {
-        this.userid = userid;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getOtp() {
-        return otp;
-    }
-
-    public void setOtp(String otp) {
-        this.otp = otp;
-    }
-
-// Constructors, getters, and setters
-
-
-    // Add relationships with other entities if necessary
-
-    // Getters and setters
 }
